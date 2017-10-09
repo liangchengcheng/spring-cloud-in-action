255815235f163057738c01d8deeeb436d9fc2f12
liangchengcheng <1038127753@qq.com>
2017-10-10 07:35:27 +0800

    add some file
    add some file

---
+++ hello-service/src/main/java/com/didispace/indicator/RocketMQHealthIndicator.java
@@ -0,0 +1,25 @@
+package com.didispace.indicator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+/**
+ * 健康指示器  indicator“指示器，仪表盘”
+ */
+@Component
+public class RocketMQHealthIndicator implements HealthIndicator{
+
+    @Override
+    public Health health() {
+        int errorCode = check();
+        if (errorCode != 0){
+            return Health.down().withDetail("Error Code",errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check(){
+        return 0;
+    }
+}

98159c1d47e65482f1415e2f89c78053cc9cdaa0
liangchengcheng <1038127753@qq.com>
2017-10-10 07:26:00 +0800

    add some java class file
     add some java class file

---
+++ feign-consumer/src/main/java/com/didispace/web/RefactorHelloService.java
@@ -0,0 +1,7 @@
+package com.didispace.web;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+@FeignClient(value = "HELLO-SERVICE")
+public interface RefactorHelloService extends HelloService{
+}

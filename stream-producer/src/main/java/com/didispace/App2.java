3eb0cfd50df781de03c20de8046773977e029c5c
liangchengcheng <1038127753@qq.com>
2017-10-19 06:49:10 +0800

    add some file
    add some file

---
+++ stream-producer/src/main/java/com/didispace/App2.java
@@ -0,0 +1,9 @@
+package com.didispace;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App2 {
+    private static Logger logger = LoggerFactory.getLogger(HelloApplication.class);
+
+}
